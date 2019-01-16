29261d4538a427b64353101cd61e504cc5c0e135
Ta David Yu <d366095@gmail.com>
2019-01-16 16:21:05 +0800

    Add fonts, update page

--- src/modules/About.js
+++ src/modules/About.js
@@ -9,15 +9,11 @@ class About extends Component {
   render() {
     return (
       <div>
-        <h2>HELLO</h2>
-        <p>Cras facilisis urna ornare ex volutpat, et
-        convallis erat elementum. Ut aliquam, ipsum vitae
-        gravida suscipit, metus dui bibendum est, eget rhoncus nibh
-        metus nec massa. Maecenas hendrerit laoreet augue
-        nec molestie. Cum sociis natoque penatibus et magnis
-        dis parturient montes, nascetur ridiculus mus.</p>
+        <h2>ABOUT ME</h2>
+        <p>My name is Ta Yu. You can call me David Yu.</p>
+        <p>I am a game developer and a programmer.</p>
  
-        <p>Duis a turpis sed lacus dapibus elementum sed eu lectus.</p>
+        <p>Hope you have a good day!</p>
       </div>
     );
   }
--- src/modules/Work.js
+++ src/modules/Work.js
@@ -9,16 +9,15 @@ class Work extends Component {
   render() {
     return (
       <div>
-        <h2>STUFF</h2>
-        <p>Mauris sem velit, vehicula eget sodales vitae,
-        rhoncus eget sapien:</p>
-        <ol>
+        <h2>MY WORKS</h2>
+        <p>Following are some of the games I made:</p>
+        <ul>
           <li>Nulla pulvinar diam</li>
           <li>Facilisis bibendum</li>
           <li>Vestibulum vulputate</li>
           <li>Eget erat</li>
           <li>Id porttitor</li>
-        </ol>
+        </ul>
       </div>
     );
   }
