f1ad066316454f452695396eb84f19683a7e156f
Ta David Yu <d366095@gmail.com>
2019-01-16 20:34:58 +0800

    Example tab

--- src/modules/About.js
+++ src/modules/About.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
- 
+
 class About extends Component {
 
   componentDidMount() {
@@ -10,8 +10,8 @@ class About extends Component {
     return (
       <div>
         <h2>ABOUT ME</h2>
-        <p>My name is Ta Yu. You can call me David Yu.</p>
-        <p>I am a game developer and a programmer.</p>
+        <div>My name is Ta Yu. You can call me David Yu.</div>
+        <div>I am a game developer and a programmer.</div>
  
         <p>Hope you have a good day!</p>
       </div>
--- src/modules/Main.js
+++ src/modules/Main.js
@@ -1,32 +1,50 @@
 import React, { Component } from "react"
 import { Route, NavLink, HashRouter } from "react-router-dom";
-
-import '../css/Main.css';
+import { Tabs, Tab, MuiThemeProvider, createMuiTheme } from '@material-ui/core';
 
 import About from "./About";
 import Work from "./Work";
 import Contact from "./Contact";
 
+import '../css/Main.css';
+
+const theme = createMuiTheme(
+    {
+        typography: {
+            fontFamily: "m6x11",
+            fontSize: 32
+        }
+    }
+)
+
 class Main extends Component {
     render() {
         return (
-            <HashRouter>
-                <div>
-                    <h1 className="header">
-                        Ta David Yu
-                    </h1>
-                    <ul className="header">
-                        <li><NavLink to="/">About</NavLink></li>
-                        <li><NavLink to="/work">Work</NavLink></li>
-                        <li><NavLink to="/contact">Contact</NavLink></li>
-                    </ul>
-                    <div className="content">
-                        <Route exact path="/" component={About} />
-                        <Route path="/work" component={Work} />
-                        <Route path="/contact" component={Contact} />
+            <MuiThemeProvider theme={theme}>
+                <HashRouter>
+                    <div>
+                        <h1 className="header">
+                            Ta David Yu
+                        </h1>
+
+                        <ul className="header">
+                            <li><NavLink to="/">About</NavLink></li>
+                            <li><NavLink to="/work">Work</NavLink></li>
+                            <li><NavLink to="/contact">Contact</NavLink></li>
+                        </ul>
+                        <Tabs>
+                            <Tab label="About" component={NavLink} to="/"/>
+                            <Tab label="Work" component={NavLink} to="/work"/>
+                            <Tab label="Contact" component={NavLink} to="/contact"/>
+                        </Tabs>
+                        <div className="content">
+                            <Route exact path="/" component={About} />
+                            <Route path="/work" component={Work} />
+                            <Route path="/contact" component={Contact} />
+                        </div>
                     </div>
-                </div>
-            </HashRouter>
+                </HashRouter>
+            </MuiThemeProvider>
         );
     }
 }
