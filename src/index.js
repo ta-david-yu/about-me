f318b3858c129b13b80b849942b1ddb6105f3729
Ta David Yu <d366095@gmail.com>
2019-01-18 19:01:53 +0800

    Use HashRouter instead of BrowserRouter

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import Main from "./modules/Main";
 
 ReactDOM.render(
-    <BrowserRouter basename={process.env.PUBLIC_URL}>
+    <HashRouter>
     <Main />
-    </BrowserRouter>,
+    </HashRouter>,
     document.getElementById("root")
 )
