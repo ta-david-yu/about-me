5a960f9e9d22dcaac5aa7177e4ccfa6803fb5635
Ta David Yu <d366095@gmail.com>
2019-01-19 07:17:37 +0800

    Add some item template

--- src/modules/About.js
+++ src/modules/About.js
@@ -14,6 +14,89 @@ const educations = [
   }
 ];
 
+const experiences = [
+  {
+    "Title": "Game Programmer Summer Intern",
+    "Company": "Akatsuki Inc.",
+    "Duration": "2017.8-2017.9",
+    "Link": "https://aktsk.com.tw/intern/"
+  },
+  {
+    "Title": "Test",
+    "Company": "Akatsuki Inc.",
+    "Duration": "2017.8-2017.9",
+    "Link": "https://aktsk.com.tw/intern/"
+  }
+];
+
+const skills = [
+  {
+    "Name": "C/C++",
+    "Description": "Good",
+    "Type": "ProgrammingLanguage"
+  },
+  {
+    "Name": "C#",
+    "Description": "Good",
+    "Type": "ProgrammingLanguage"
+  },
+  {
+    "Name": "JavaScript",
+    "Description": "Good",
+    "Type": "ProgrammingLanguage"
+  },
+  {
+    "Name": "PHP",
+    "Description": "Good",
+    "Type": "ProgrammingLanguage"
+  },
+  {
+    "Name": "GLSL/UnityShader",
+    "Description": "Good",
+    "Type": "ProgrammingLanguage"
+  },
+  {
+    "Name": "ReactJS",
+    "Description": "Good",
+    "Type": "Tools"
+  },
+  {
+    "Name": "Unity3D",
+    "Description": "Good",
+    "Type": "Tools"
+  },
+  {
+    "Name": "git",
+    "Description": "Good",
+    "Type": "Tools"
+  },
+  {
+    "Name": "MySQL",
+    "Description": "Good",
+    "Type": "Tools"
+  },
+  {
+    "Name": "VisualStudio",
+    "Description": "Good",
+    "Type": "Tools"
+  },
+  {
+    "Name": "Chinese",
+    "Description": "Good",
+    "Type": "Language"
+  },
+  {
+    "Name": "English",
+    "Description": "Good",
+    "Type": "Language"
+  },
+  {
+    "Name": "Japanese",
+    "Description": "Good",
+    "Type": "Language"
+  }
+];
+
 class About extends Component {
 
   componentDidMount() {
@@ -21,21 +104,47 @@ class About extends Component {
   }
 
   componentWillMount() {
-    this.generateEducation();
+    this.generateTable();
   }
 
-  generateEducation() {
+  generateTable() {
     this.educations = educations.map(
       (edu) => {
         return(
           <Grid item lg md sm xs className="content box-g255">
-            <div className="degree">
+            <div className="main-text">
               <a href={edu.Link} target="_blank">
               <img alt="+" src="./img/link-x13.png" className="icon"/>
               </a> {edu.Degree} ({edu.Duration})
             </div>
-            <div className="gpa">
+            <div className="sub-text">
               GPA {edu.GPA}
+            </div>
+          </Grid>
+        );
+      }
+    );
+
+    this.experiences = experiences.map(
+      (exp) => {
+        return(
+          <Grid item lg md sm xs className="content box-g255">
+            <div className="main-text">
+              <a href={exp.Link} target="_blank">
+              <img alt="+" src="./img/link-x13.png" className="icon"/>
+              </a> {exp.Title} - {exp.Company} ({exp.Duration})
+            </div>
+          </Grid>
+        );
+      }
+    );
+
+    this.skills = skills.map(
+      (skill) => {
+        return(
+          <Grid item>
+            <div className="main-text box-g255">
+              {skill.Name}
             </div>
           </Grid>
         );
@@ -75,10 +184,20 @@ class About extends Component {
           <Grid item lg md sm xs className="content-header">
             experience
           </Grid>
-          <Grid item lg md sm xs className="content">
-          </Grid>
+          {this.experiences}
         </Grid>
 
+        <GridOffset></GridOffset>
+
+        <Grid container direction="column" lg={8} spacing={8}>
+          <Grid item lg md sm xs className="content-header">
+            skills
+          </Grid>
+          <Grid container direction="row" justify="center">
+            {this.skills}
+          </Grid>
+        </Grid>
+        
       </Grid>
     );
   }
